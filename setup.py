027de49382a0b37832525f1b770675f2f6d840db
Abraham Martin <amc203@cam.ac.uk>
2014-07-16 09:32:16 +0100

    version change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='A Django module to use the template system of the University of Cambridge: Project light',
     long_description=open('README.rst').read(),
     url='https://git.csx.cam.ac.uk/x/ucs/raven/django-ucamprojectlight.git',
-    version='1.0',
+    version='1.1',
     license='MIT',
     author='Information Systems Group, University Information Services, University of Cambridge',
     author_email='information-systems@ucs.cam.ac.uk',
